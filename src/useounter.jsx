266cd4f98d0ae75e77f67577ecc6fb7cf85cec29
Satyam Rajput <satyamrajput081998@gmail.com>
2024-11-30 04:20:53 -0500

    Change some Format of files

--- src/useounter.jsx
+++ src/useounter.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 const useCounter = () => {
   const [count, setCount] = useState(0);
-
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
 
